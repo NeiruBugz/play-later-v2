ece05f59ea0a75ad03bec73f4462970aff525120
Nail Badiullin <nbadiullin@provectus.com>
2023-09-08 08:36:12 +0400

    update protected layout, add user dropdown

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,15 +1,19 @@
-import { siteConfig } from "@/config/site"
-import { MainNav } from "@/components/main-nav"
-import { ModeToggle } from "@/components/theme-toggle"
+import { getServerSession } from "next-auth"
 
-export function SiteHeader() {
+import { siteConfig } from "@/config/site"
+import { authOptions } from "@/lib/auth"
+import { MainNav } from "@/components/main-nav"
+import { UserDropdown } from "@/components/user-dropdown"
+
+export async function SiteHeader() {
+  const session = await getServerSession(authOptions)
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-background">
       <div className="flex h-16 items-center space-x-4 px-4 md:container sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
-          <nav className="flex items-center space-x-1">
-            <ModeToggle />
+          <nav className="flex items-center gap-1 space-x-1">
+            <UserDropdown session={session} />
           </nav>
         </div>
       </div>
---
+++ components/user-dropdown.tsx
@@ -0,0 +1,80 @@
+"use client"
+
+import * as React from "react"
+import Link from "next/link"
+import { LogOut, Moon, Settings, Sun } from "lucide-react"
+import { Session } from "next-auth"
+import { signOut } from "next-auth/react"
+import { useTheme } from "next-themes"
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+function nameFirstLiterals(name: string) {
+  if (!name) {
+    return "U"
+  }
+  const [firstName, lastName] = name.split(" ")
+  return `${firstName[0]}${lastName[0]}`
+}
+
+export function UserDropdown({ session }: { session: Session | null }) {
+  const { setTheme } = useTheme()
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Avatar className="h-9 w-9 cursor-pointer rounded-md">
+          <AvatarImage
+            src={session?.user.image ?? ""}
+            alt={session?.user.email ?? ""}
+          />
+          <AvatarFallback className="h-9 w-9 rounded-md">
+            {nameFirstLiterals(session?.user.name ?? "")}
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuSub>
+          <DropdownMenuSubTrigger>
+            <Sun className="mr-2 h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute mr-2 h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <span className="sr-only">Toggle theme</span>Theme
+          </DropdownMenuSubTrigger>
+          <DropdownMenuSubContent>
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuSubContent>
+        </DropdownMenuSub>
+        <DropdownMenuPortal></DropdownMenuPortal>
+        <DropdownMenuItem disabled>
+          <Link href="/settings" className="flex items-center">
+            <Settings className="mr-2 h-4 w-4" />
+            Settings
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className="cursor-pointer" onClick={() => signOut()}>
+          <LogOut className="mr-2 h-4 w-4" />
+          Logout
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
