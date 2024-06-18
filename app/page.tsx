460b9395d344997add0d415ea12d85b1ab3682b3
Nail Badiullin <nbadiullin@provectus.com>
2024-06-18 21:16:28 +0400

    implement raw google oauth

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,13 @@
-export default function Page() {
+import { auth } from "@/auth";
+import { SignIn } from "@/src/features/sign-in";
+import { redirect } from "next/navigation";
+
+export default async function Page() {
+  const session = await auth();
+  if (session) {
+    redirect("/collection");
+  }
+
   return (
     <div className="min-h-screen flex-1">
       <section className="container flex columns-2 flex-col items-center md:h-[calc(100vh-64px-48px)] md:flex-row md:justify-center">
@@ -9,6 +18,7 @@ export default function Page() {
           <h2 className="text-3xl font-bold tracking-tighter lg:text-5xl">
             Remake is Coming
           </h2>
+          <SignIn />
         </div>
       </section>
     </div>
